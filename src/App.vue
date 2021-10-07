cb2e673dff9c396aba1beef9c96a2008d71bb8e1
balazs230 <bacs.balazs@yahoo.com>
2021-10-07 21:52:46 +0300

    commit vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Nav />
     <router-view />
   </div>
 </template>
+
+<script>
+import Nav from './components/Nav.vue'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    Nav
+  },
+  async created() {
+    const response = await axios.get('user')
+
+    this.$store.dispatch('user', response.data)
+  },
+}
+</script>
+
 
 <style>
 #app {
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 
 Vue.use(VueRouter);
 
@@ -20,10 +22,21 @@ const routes = [
       return import(/* webpackChunkName: "about" */ "../views/About.vue");
     },
   },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
 ];
 
 const router = new VueRouter({
   routes,
+  mode: 'history'
 });
 
 export default router;
