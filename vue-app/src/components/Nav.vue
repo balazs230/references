6709eef6ce5fcd81faf15409ea18a3a8406b6b06
Balazs Bacs <bacs.balazs@yahoo.com>
2021-10-08 14:37:13 +0000

    Upload New File

---
+++ vue-app/src/components/Nav.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <div v-if="!user">
+         <router-link to="/login">Login</router-link> |
+        <router-link to="/register">Register</router-link> 
+      </div>
+      <div v-if="user">
+         <a href="javascript:void(0)" @click="handleClick" class="nav-link">Logout</a>
+      </div>
+      
+    </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+    export default {
+        name: 'Nav',
+        methods: {
+            handleClick() {
+                localStorage.removeItem('token')
+                this.$store.dispatch('user', null)
+                this.$router.push('/')
+            }
+        },
+        computed: {
+            ...mapGetters(['user'])
+        }
+    }
+</script>
+
+<style  scoped>
+
+</style>
