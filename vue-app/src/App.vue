f28230d6e8d6523ac85b73c2788c7f84fa6d7b44
Balazs Bacs <bacs.balazs@yahoo.com>
2021-10-08 14:40:43 +0000

    Upload New File

---
+++ vue-app/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <Nav />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav.vue'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    Nav
+  },
+  async created() {
+    const response = await axios.get('user')
+
+    this.$store.dispatch('user', response.data)
+  },
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
