b998090d7f8c28977643e9ca7d7f65f6ca8f094a
Balazs Bacs <bacs.balazs@yahoo.com>
2021-10-08 14:39:49 +0000

    Upload New File

---
+++ vue-app/src/views/Register.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="auth">
+    <h1>Register</h1>
+    <div class="error"></div>
+    <div class="container">
+    <form @submit.prevent="handleSubmit">
+        <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="name" v-model="name" class="form-control" placeholder="Enter name" id="name">
+    </div>
+        <div class="form-group">
+        <label for="pwd">Password:</label>
+        <input type="password" v-model="pwd" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+        </div>
+        <button type="submit" class="btn btn-primary btn-warning">Register</button>
+    </form>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Register',
+    data() {
+        return {
+            name: '',
+            pwd: ''
+        }
+    },
+    methods: {
+       async handleSubmit() {    
+          const response = await axios.post('register', {
+                name: this.name,
+                pwd: this.pwd
+            })
+
+            console.log(response)
+            this.$router.push('/login') //redirecting
+        }
+    }
+}
+</script>
